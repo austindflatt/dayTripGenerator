d9c9b84f4d0274e424b554cd82b84d76381679ae
Austin Flatt <austindflatt@gmail.com>
2021-04-30 21:46:31 -0400

    Added arrays for trip

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 "use strict";
+
+let destinations = ['Louisville', 'Lexington', 'Cincinatti', 'Florence'];
+let restraunts = ['McDonalds', 'Burger King', 'Chick fil A', 'Cheesecake Factory'];
+let transportation = ['Car', 'Bus', 'Uber', 'Lyft'];
+let entertainment = ['Laser Tag', 'Basketball', 'Paintball', 'Video Games'];
